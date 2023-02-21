5e06d5592a32db5ff73985558e8f95256d212444
BBecker8 <97985236+BBecker8@users.noreply.github.com>
2023-02-21 00:28:03 +0000

    case class push2

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -1,5 +1,11 @@
 package dfa // leave this line in the file
 
 case class State(label: String):
+    def stateName = label
+    override def toString(): String = lable
 
 case class Transition(from: State, symbol: Char, to: State):
+    def previosState = from
+    def value = symbol
+    def nextState = to
+    
