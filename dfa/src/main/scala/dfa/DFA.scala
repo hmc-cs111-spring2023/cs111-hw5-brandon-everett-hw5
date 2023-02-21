2c2d515187f645de7e92e81a19349d38b9893742
BBecker8 <97985236+BBecker8@users.noreply.github.com>
2023-02-21 00:35:39 +0000

    outline calsses

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -8,4 +8,10 @@ case class Transition(from: State, symbol: Char, to: State):
     def previosState = from
     def value = symbol
     def nextState = to
+
+class DFA (val states: Set[State], val transitions: Set[Transition], val startState: State, val acceptingStates: Set[State]):
     
+    def accepts (str: String): Boolean = {
+
+    }
+
