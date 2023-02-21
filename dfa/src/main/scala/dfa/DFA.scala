ee06962f363c55719b06d5a2334a70213a4fd03a
BBecker8 <97985236+BBecker8@users.noreply.github.com>
2023-02-21 01:16:06 +0000

    DFA final commit

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -1,17 +1,19 @@
 package dfa // leave this line in the file
 
-case class State(label: String):
-    def stateName = label
-    override def toString(): String = lable
+case class State(label: String)
 
-case class Transition(from: State, symbol: Char, to: State):
-    def previosState = from
-    def value = symbol
-    def nextState = to
+case class Transition(from: State, to: State, symbol: Char)
 
-class DFA (val states: Set[State], val transitions: Set[Transition], val startState: State, val acceptingStates: Set[State]):
-    
-    def accepts (str: String): Boolean = {
-
+class DFA(val states: Set[State], val transitions: Set[Transition], val start: State, val accept: Set[State]) {
+  def accepts(input: String): Boolean = {
+    var currentState = start
+    for (symbol <- input) {
+      val transitionOpt = transitions.find(t => t.from == currentState && t.symbol == symbol)
+      transitionOpt match {
+        case Some(transition) => currentState = transition.to
+        case None => false
+      }
     }
-
+    accept.contains(currentState)
+  }
+}
